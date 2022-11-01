eb690d7df557acd5c0be5b649db4c26fe98f1cec
gowri <balugowri244@gmail.com>
2022-11-01 15:15:58 +0530

    ts file

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.scss']
+  styleUrls: ['./calculator.component.scss'],
 })
 export class CalculatorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  Input = '0';
+  CurrentValue = '';
+  EnteredNumber(value: string) {
+    this.CurrentValue = this.CurrentValue + value;
+    this.Input = this.CurrentValue;
   }
-
+  equal() {
+    this.Input = eval(this.CurrentValue);
+    this.CurrentValue = this.Input;
+  }
+  cancle() {
+    this.CurrentValue = '';
+    this.Input = '0';
+  }
+  del() {
+    this.CurrentValue = this.CurrentValue.slice(0, -1);
+    this.Input = this.CurrentValue;
+    if (this.Input == '') {
+      this.Input = '0';
+    }
+  }
+  constructor() {}
+  ngOnInit(): void {}
 }
